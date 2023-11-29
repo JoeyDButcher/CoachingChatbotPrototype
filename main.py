6b05e4502d35d124518ffc22396284e1d19e7e73
Joey <joeybutcher1000@gmail.com>
2023-11-29 17:47:20 +0000

    Creation of Repo
    self explanatory

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hello! Welcome to this brand new prototype Coaching Chatbot. Please keep in mind: This is NOT a generative tool."
+      "I will not be able to provide any real feedback for you, due to my current limitations. This tool in "
+      "its current form is designed to be more of a self reflection tool.\n"
+      "Everything we talk about to day will be based off of the 'GROW' method of Coaching. As such: I will now ask you"
+      "four questions about what it is you're looking to achieve with Coaching.\n")
+G = input("First up: What is your Goal you are looking to achieve? For some help with this, consider the following: "
+          "What precisely are you looking to achieve and how would you define your goal?\n")
+print("Okay! That all makes sense! Don't worry by the way, I'll review your answers with you later.")
+R = input("Next: What is the Reality of your current situation? Consider the following: What's happening in your life "
+          "right now? How will your situation impact your goal? What resources are available to you?\n")
+print("Sounds good! We're half way there, keep it up!")
+O = input("Alright, what options do you have available to you? For help: Consider fully what options there are"
+          "before you, how might each of these different options play out and also how you would rank all of these "
+          "options.\n")
+print("Alright, we're almost done now, just one more section to think about!")
+W = input("Finally: What's next? How are you commit to a path forward with this? Think about: How committed you are "
+          "to this goal, what are the first steps for you to take on this and what might get in your way\n")
+print("With that, we're done! Just to confirm:\n"
+      "Your Goal is: " + G + "!\n"
+      "Your Reality is: " + G + "!\n"
+      "Your Options are: " + G + "!\n"
+      "Your What Next is: " + G + "!\n"
+      "While I'm not able to provide tailored advise for you, I hope this opportunity for self reflection has been"
+      "helpful!")
